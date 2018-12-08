cac27a30db1c725c6fe755aadb2667b0cfadf182
Aaron Woo <aaronhwoo@gmail.com>
2018-12-08 11:06:08 -0500

    update poll
    cleaned up variable and added comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,20 +9,19 @@ csvpath = os.path.join("Resources", "election_data.csv")
 total_votes = 0
 candidates = {}
 percentage_vote = 0
-candidate_vote_counter = 0
 
 #open data file and skip header
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader)
-
+#tally up dictionary total dynamically
     for row in csvreader:
         total_votes = total_votes + 1
         current_vote = row[2]
         if current_vote not in candidates:
             candidates[current_vote] = 1
         else: 
-            candidates[current_vote] = candidates[current] + 1
+            candidates[current_vote] = candidates[current_vote] + 1
     print(total_votes)
     print(candidates)
             
