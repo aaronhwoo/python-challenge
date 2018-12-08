38939630aba355a28bf640ed6bd0d95e4ac7532e
Aaron Woo <aaronhwoo@gmail.com>
2018-12-07 23:05:33 -0500

    good enough
    semi-fixed, will output everything assignment wants but using rows instead of tuples

--- PyBank/main.py
+++ PyBank/main.py
@@ -49,16 +49,31 @@ row3 = [f"Total: ${net_profit}"]
 row4 = [f"Average Change: ${avg_profit}"]
 row5 = [f'Greatest Increase in Profits: {g_increase_date} (${g_increase})']
 row6 = [f'Greatest Decrease in Profits: {g_decrease_date} (${g_decrease})']
-newline = ["|"]
 
-export = zip(row0, newline, row1, newline, row2, newline, row3, newline, row4, newline, row5, newline, row6)
+print(row0)
+print(row1)
+print(row2)
+print(row3)
+print(row4)
+print(row5)
+print(row6)
 
-for analysis in export:
-    print(analysis)
+#export = zip(row0, row1, row2, row3, row4, row5, row6)
+
+#can't get this to work in a formatted way
+#for analysis in export: 
+#    print(analysis)
 
 output_file = os.path.join("financial_analysis.csv")
 
 with open(output_file, "w", newline="") as datafile:
     writer = csv.writer(datafile)
 
-    writer.writerows(export)
+    #writer.writerows(export) doesn't seem to work but it works with individual row, problem in tuple?
+    writer.writerow(row0)
+    writer.writerow(row1)
+    writer.writerow(row2)
+    writer.writerow(row3)
+    writer.writerow(row4)
+    writer.writerow(row5)
+    writer.writerow(row6)
