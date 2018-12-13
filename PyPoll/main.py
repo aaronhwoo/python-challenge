2be1b913b7ac4b4237b500308615a5bbf41fbbf3
Aaron Woo <aaronhwoo@gmail.com>
2018-12-13 10:58:55 -0500

    poll mostly final update
    poll finished, reran dictionary creator to work around the string problem

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -35,14 +35,30 @@ with open(output_file, "w", newline="") as text_file:
     text_file.write("---------------------------\n")
     for candidates, votes in candidates.items():
         text_file.write(f'Candidates : {candidates}     Votes: {votes}     Percent:{votes*100/total_votes}%\n')
-    text_file.write(f'The winner is {winner}')      
+    text_file.write(f'The winner is {winner}')  
+    
+print(type(candidates))
+
+candidates = {}
+#open data file and skip header
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
+#tally up dictionary total dynamically
+    for row in csvreader:
+        total_votes = total_votes + 1
+        current_vote = row[2]
+        if current_vote not in candidates:
+            candidates[current_vote] = 1
+        else: 
+            candidates[current_vote] = candidates[current_vote] + 1
+
 
 print(type(candidates))
 print("Election Results")
 print("---------------------------")
 for candidates, votes in candidates.items():
-    print(type(candidates))
-    print(f'Candidate : {c_candidate}     Votes: {c_votes}     Percent:{c_votes*100/total_votes}%')
+    print(f'Candidate : {candidates}     Votes: {votes}     Percent:{votes*100/total_votes}%')
     if votes > winner_votes:
         winner = candidates
         winner_votes = votes
